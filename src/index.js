0aad9784e1e391eefdca033400ba527cfa04c740
Patryk <patryk@mac-mini-patryk.home>
2018-06-27 14:04:52 +0200

    creating basic folders structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import App from './Components/App';
+
+import './css/style.css';
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
