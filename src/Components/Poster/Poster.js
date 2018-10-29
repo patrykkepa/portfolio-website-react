de0d03c47ce3b4397e0f09a1211b4751cee0bc96
Patryk <patryk@Mac-mini-Patryk.local>
2018-10-29 19:56:20 +0100

    Making Welcome About Work and Contact responsive for 850 and 480px

--- src/Components/Poster/Poster.js
+++ src/Components/Poster/Poster.js
@@ -4,22 +4,23 @@ import './Poster.css';
 
 const Poster = (props) => {
     return(
-        <div id="" className="o-posterContainer">
+        <div id={props.id} className="o-posterContainer">
             
-            <div className="o-poster">
+            
                 <div className="e-posterTitle">
                     <i className="fas fa-arrow-right"></i>
                     <h1>{props.title}</h1>
                 </div>
-                <div id={props.id} className="e-posterBackground">
-                </div>
-                <div className="e-posterImage">
-                </div>
+
                 <div className="e-posterDescription">
                     <p>{props.text}</p>
                     {props.icon}
                 </div>
-            </div>
+                
+                <div className="e-posterImage"></div>
+
+                
+            
             
         </div>
     )
